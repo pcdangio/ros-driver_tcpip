1034f228448492c12a0f86893efd560b2c66cd14
Paul D'Angio <pcdangio@gmail.com>
2019-07-29 21:32:02 -0400

    Added documentation to connection_type enum

--- src/connection_type.h
+++ src/connection_type.h
@@ -1,10 +1,15 @@
+/// \file connection_type.h
+/// \brief Defines the connection_type enumeration.
 #ifndef CONNECTION_TYPE_H
 #define CONNECTION_TYPE_H
 
+///
+/// \brief An enumeration of TCP/IP connection protocol types.
+///
 enum class connection_type
 {
-    TCP = 0,
-    UDP = 1
+    TCP = 0,    ///< TCP Protocol
+    UDP = 1     ///< UDP Protocol
 };
 
 #endif // CONNECTION_TYPE_H
