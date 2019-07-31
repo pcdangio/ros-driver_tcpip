e3c286c81141aefeae9d1236704e1b3c6112151a
Paul D'Angio <pcdangio@gmail.com>
2019-07-31 19:43:13 -0400

    Changed original enum to protocol

---
+++ src/protocol.h
@@ -0,0 +1,15 @@
+/// \file protocol.h
+/// \brief Defines the protocol enumeration.
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+///
+/// \brief An enumeration of TCP/IP connection protocol types.
+///
+enum class protocol
+{
+    TCP = 0,    ///< TCP Protocol
+    UDP = 1     ///< UDP Protocol
+};
+
+#endif // PROTOCOL_H
