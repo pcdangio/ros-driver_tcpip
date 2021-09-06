56623c5076454f37c542db3df430e60864d8fd77
Paul D'Angio <pcdangio@gmail.com>
2021-09-06 17:36:51 -0400

    tcp_server: implemented async accepting

--- src/tcp_server.cpp
+++ src/tcp_server.cpp
@@ -1,15 +1,74 @@
 #include "tcp_server.hpp"
 
+#include <ros/console.h>
+
+#include <boost/bind.hpp>
+#include <boost/asio/placeholders.hpp>
+
 using namespace driver_modem;
 
 // CONSTRUCTORS
-tcp_server_t::tcp_server_t(boost::asio::io_service& io_service, uint32_t id)
+tcp_server_t::tcp_server_t(boost::asio::io_service& io_service, uint32_t id, std::function<void(uint32_t, boost::asio::ip::tcp::socket*)> connection_callback)
     : m_acceptor(io_service),
       m_id(id)
 {
-
+    // Store connection callback.
+    tcp_server_t::m_callback_connection = connection_callback;
 }
 tcp_server_t::~tcp_server_t()
 {
+    tcp_server_t::stop();
+}
 
+// CONTROL
+bool tcp_server_t::start(driver_modem_msgs::endpoint& local_endpoint)
+{
+
+}
+void tcp_server_t::stop()
+{
+
+}
+
+// ASIO ACCEPTOR
+void tcp_server_t::async_accept()
+{
+    // Create a new socket for accepting connections.
+    tcp_server_t::m_socket = new boost::asio::ip::tcp::socket(tcp_server_t::m_acceptor.get_io_service());
+
+    // Start a new asynchronous accept operaton.
+    tcp_server_t::m_acceptor.async_accept(*tcp_server_t::m_socket, boost::bind(&tcp_server_t::accept_callback, this, boost::asio::placeholders::error));
+}
+void tcp_server_t::accept_callback(const boost::system::error_code &error)
+{
+    // Check if there was an error.
+    if(!error)
+    {
+        // Raise the connection callback.
+        tcp_server_t::m_callback_connection(tcp_server_t::m_id, tcp_server_t::m_socket);
+
+        // This passes the ownership of the socket through the callback.
+        tcp_server_t::m_socket = nullptr;
+    }
+    else
+    {
+        // An error occured.
+
+        // Clean up socket.
+        delete tcp_server_t::m_socket;
+        tcp_server_t::m_socket = nullptr;
+
+        // Check if operation was aborted.
+        if(error == boost::asio::error::operation_aborted)
+        {
+            // Quit accept loop.
+            return;
+        }
+
+        // Report error.
+        ROS_ERROR_STREAM("tcp server " << tcp_server_t::m_id << " failed to accept connection (" << error.message() << ")");
+    }
+
+    // Restart accept loop.
+    tcp_server_t::async_accept();  
 }
--- src/tcp_server.hpp
+++ src/tcp_server.hpp
@@ -3,7 +3,11 @@
 #ifndef DRIVER_MODEM___TCP_SERVER_H
 #define DRIVER_MODEM___TCP_SERVER_H
 
+#include <driver_modem_msgs/endpoint.h>
+
 #include <boost/asio/ip/tcp.hpp>
+
+#include <functional>
 
 namespace driver_modem {
 
@@ -11,18 +15,39 @@ namespace driver_modem {
 class tcp_server_t
 {
 public:
-    // CONSTRUCOTRS
+    // CONSTRUCTORS
     /// \brief Instantiates a new tcp_server_t.
     /// \param io_service The application's io_service instance.
     /// \param id The unique ID of the TCP server.
-    tcp_server_t(boost::asio::io_service& io_service, uint32_t id);
+    tcp_server_t(boost::asio::io_service& io_service, uint32_t id, std::function<void(uint32_t, boost::asio::ip::tcp::socket*)> connection_callback);
     ~tcp_server_t();
 
+    // CONTROL
+    /// \brief Starts the TCP server.
+    /// \param local_endpoint The local endpoint to start the server on.
+    /// \returns TRUE if the server started successfully, otherwise FALSE.
+    bool start(driver_modem_msgs::endpoint& local_endpoint);
+    /// \brief Stops the TCP server.
+    void stop();
+
 private:
-    /// \brief The TCP acceptor for the server.
-    boost::asio::ip::tcp::acceptor m_acceptor;
+    // VARIABLES
     /// \brief The unique ID of the server.
     const uint32_t m_id;
+
+    // ASIO ACCEPTOR
+    /// \brief The TCP acceptor for the server.
+    boost::asio::ip::tcp::acceptor m_acceptor;
+    /// \brief The new socket for the next connection.
+    boost::asio::ip::tcp::socket* m_socket;
+    /// \brief Starts asynchronously accepting connections.
+    void async_accept();
+    /// \brief Handles the acceptance of new connections.
+    void accept_callback(const boost::system::error_code &error);    
+
+    // CALLBACKS
+    /// \brief Stores the external callback for when new connections are made.
+    std::function<void(uint32_t, boost::asio::ip::tcp::socket*)> m_callback_connection;
 };
 
 }
